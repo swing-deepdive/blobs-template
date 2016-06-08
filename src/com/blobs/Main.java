a6c96e84190db0d3654ff16c6c6657b52a731b67
Rene Brandel <renebrandel@outlook.com>
2016-06-08 03:54:43 +0200

    Updated main to bring on the chainz for the stripper of the MVC Club

--- src/com/blobs/Main.java
+++ src/com/blobs/Main.java
@@ -1,12 +1,17 @@
 package com.blobs;
 
+import com.blobs.controller.Controller;
+import com.blobs.model.Game;
 import com.blobs.view.View;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new View();
+		View view = new View();
+		Game game = new Game(view);
+		Controller controller = new Controller(game);
+		view.setController(controller);
 	}
 
 }
