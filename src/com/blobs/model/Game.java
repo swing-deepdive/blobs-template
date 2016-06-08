444f43204e0c6e76dc2beb29127057ad8846e9db
Rene Brandel <renebrandel@outlook.com>
2016-06-08 03:19:47 +0200

    Updated to accommodate the new IView interface

--- src/com/blobs/model/Game.java
+++ src/com/blobs/model/Game.java
@@ -33,26 +33,27 @@ public class Game implements IModel{
 	public void killBlob(int id) {
 		this.score += this.field.getBlobScore(id);
 		this.field.removeBlob(id);
-		//call to view
+		this.view.hideSlot(id);
+		this.view.updateScore(this.score);
 	}
 	
 	public void gameOver() {
 		System.out.println("Game Over");
 		this.gameTimer.cancel();
 		this.spawnTimer.cancel();
-		// gameOver
+		this.view.gameOver();
 	}
 	
 	public void addBlob() {
 		int location = this.field.addBlob();
-		// call to view
+		this.view.showSlot(location);
 	}
 
 	@Override
 	public void startGame() {
 		this.gameTimer.scheduleAtFixedRate(new CheckGameTask(this, this.field), 50, 50);
 		this.spawnTimer.schedule(new NewBlobTask(this, this.spawnTimer, 2000), 2000);
-		
+		this.view.gameStarted();
 	}
 
 	@Override
@@ -63,7 +64,7 @@ public class Game implements IModel{
 
 	@Override
 	public void submitHighscore(String name, int score) {
-		// TODO Auto-generated method stub
-		
+		// Network
+		this.view.highscoreSubmitted(score, name);
 	}
 }
