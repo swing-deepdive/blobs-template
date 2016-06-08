2d509570a82796c434616ed54353031b7e2d4f1e
Rene Brandel <renebrandel@outlook.com>
2016-06-08 07:31:27 +0200

    AzureConnector completed

--- src/com/blobs/network/AzureConnector.java
+++ src/com/blobs/network/AzureConnector.java
@@ -1,26 +1,32 @@
 package com.blobs.network;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class AzureConnector {
-	
-	public static void main(String[] args) {
-		new AzureConnector().getTable("highscore");
+
+	private String url;
+	private String gameId;
+
+	public AzureConnector(String gameId) {
+		this.url = API_URL + "&id=" + gameId;
+		this.gameId = gameId;
 	}
-	
-	static final String TABLE_URL = "http://blobs-swing.azurewebsites.net/tables/%s?zumo-api-version=2.0.0";
-	
-	public void getTable(String table) {
-		String url = String.format(TABLE_URL, table);
+
+	static final String API_URL = "http://blobs-swing.azurewebsites.net/api/highscore?zumo-api-version=2.0.0";
+	static final String TABLE_URL = "http://blobs-swing.azurewebsites.net/tables/highscore?zumo-api-version=2.0.0";
+	public void getScores() {
 		try {
 			URL obj = new URL(url);
 			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-			BufferedReader in = new BufferedReader(
-			        new InputStreamReader(con.getInputStream()));
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 			String inputLine;
 			StringBuffer response = new StringBuffer();
 
@@ -28,11 +34,47 @@ public class AzureConnector {
 				response.append(inputLine);
 			}
 			in.close();
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
+	public void putScore(String name, int score) {
+		String json = "{\"Name\":\"" + name + "\", \"Score\":" + Integer.toString(score) + ", \"game_id\":"
+				+ this.gameId + "}";
+		byte[] postData       = json.getBytes( StandardCharsets.UTF_8 );
+		int    postDataLength = postData.length;
+		URL url;
+		try {
+			url = new URL( TABLE_URL );
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();           
+			conn.setDoOutput( true );
+			conn.setInstanceFollowRedirects( false );
+			conn.setRequestMethod( "POST" );
+			conn.setRequestProperty( "Content-Type", "application/json"); 
+			conn.setRequestProperty( "charset", "utf-8");
+			conn.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
+			conn.setUseCaches( false );
+			try( DataOutputStream wr = new DataOutputStream( conn.getOutputStream())) {
+			   wr.write( postData );
+			}
+			conn.connect();
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
 }
